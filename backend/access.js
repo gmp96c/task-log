825b13efc77d135a78ba2d1ab6a82edaa4ca4239
gmp96c <61660851+gmp96c@users.noreply.github.com>
2021-06-28 19:25:29 -0500

    add log working

--- backend/access.js
+++ backend/access.js
@@ -1,19 +1,19 @@
 const userIsAdmin = ({ authentication: { item: user } }) =>
-  Boolean(user && user.isAdmin);
+    Boolean(user && user.isAdmin);
 const userOwnsItem = ({ authentication: { item: user } }) => {
-  if (!user) {
-    return false;
-  }
-  // Instead of a boolean, you can return a GraphQL query:
-  // https://www.keystonejs.com/api/access-control#graphqlwhere
-  return { id: user.id };
+    if (!user) {
+        return false;
+    }
+    // Instead of a boolean, you can return a GraphQL query:
+    // https://www.keystonejs.com/api/access-control#graphqlwhere
+    return { id: user.id };
 };
 const userExists = ({ authentication: { item: user } }) => !!user;
 
 const userIsAdminOrOwner = (auth) => {
-  const isAdmin = userIsAdmin(auth);
-  const isOwner = userOwnsItem(auth);
-  return isAdmin || !!isOwner;
+    const isAdmin = userIsAdmin(auth);
+    const isOwner = userOwnsItem(auth);
+    return isAdmin || !!isOwner;
 };
 const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner, userExists };
 module.exports = { access };
