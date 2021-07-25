710c92c76631f3e9ca3b96631fb5254b83c73ef1
gmp96c <61660851+gmp96c@users.noreply.github.com>
2021-07-25 18:52:09 -0500

    show tip menu on log entry

--- frontend/src/components/Tips.tsx
+++ frontend/src/components/Tips.tsx
@@ -21,7 +21,7 @@ export const Tips: React.FC<TipProps> = ({ task, mode }: TipProps) => {
                         </li>
                     ))}
             </ul>
-            {mode === 'Settings' && (
+            {['Settings', 'Log'].includes('mode') && (
                 <button
                     type="button"
                     onClick={() => {
